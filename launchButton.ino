abf5ea7f4060608e81cf03472587d5ea92bf903f
fitzblixen <gsolomentsev@icloud.com>
2022-03-31 14:48:22 +0200

    Update launchButton.ino

--- launchButton.ino
+++ launchButton.ino
@@ -1,8 +1,9 @@
-// 2022-03-10 Gleb Solomentsev
-// First iteration of funcitoning launch button to being Roast sequence (for now LED blink)
+// 2022-03-31 Gleb Solomentsev
+// First iteration of functioning launch button to begin Roast sequence (for now LED blink)
+// including kill option (kill option works but seems a bit buggy, work this out)
 //
 
-const int ledPin =6;
+const int ledPin =3;
 const int butPin =13;
 
 
@@ -16,17 +17,25 @@ int butPinPrev = 0x0;//LOW
 int butPinCurr = 0x0;//LOW
  
 void loop()
-{  
+{
   if (digitalRead(butPin)== HIGH){
     butPinCurr = HIGH;
   }
-  
-  if (butPinPrev == LOW & butPinCurr == HIGH){
-    
+  if (digitalRead(butPin)== HIGH & butPinCurr == HIGH){
+    butPinCurr = LOW;
+  }
+
+  if (butPinPrev == LOW & butPinCurr == HIGH) { 
     analogWrite(ledPin,80);
     delay(1000); // Wait for 1000 millisecond(s)
-  	analogWrite(ledPin, 10);
-  	delay(1000); // Wait for 1000 millisecond(s)digitalRead(butPin) == LOW){
+    analogWrite(ledPin, 10);
+    delay(1000); // Wait for 1000 millisecond(s)digitalRead(butPin) == LOW)
+
   }
+  
+  else if (butPinPrev == HIGH & butPinCurr == LOW){
+    analogWrite(ledPin, 10);
+  }
+
 
 }
